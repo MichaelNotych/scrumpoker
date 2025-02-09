685fe601a0722ec1c2f2f5e477ebf658345e1bbc
MichaelNotych <notychweb@gmail.com>
2025-02-09 13:32:53 +0700

    feat: update server url

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			'/api': {
-				target: 'http://127.0.0.1:5000',
+				target: 'https://scrumpoker-server.onrender.com/',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ''),
 			},
